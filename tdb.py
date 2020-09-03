593fef0e406a9731d442f86a38752ec838044d9b
Soubhik Biswas <blackc8official@gmail.com>
2020-09-03 10:21:40 +0530

    Update tdb.py

--- tdb.py
+++ tdb.py
@@ -7,25 +7,25 @@ class tdb:
     self.file = open(self.name, "r+")
     self.db = self.load()
 
-  def commit(self, db=None):
+  def commit(self, db=None): # save the changes to file
     if db != None: self.db = db
-    self.file.truncate(0)
+    self.file.truncate(0) # clear the file
     for key in self.db:
       self.file.write("{};{};\n".format(key,self.db[key]))
 
-  def load(self):
+  def load(self): # load the db in a variable
     data = {}
     for line in self.file.readlines():
       line=line.split(";")
       data[line[0]] = line[1]
     return data
 
-  def put(self, key, value):
+  def put(self, key, value): # add entry
     self.db[key] = value
 
-  def get(self, key):
+  def get(self, key): # get entry
     return self.db[key]
 
-  def rm(self, key):
-    del self.db[key]
+  def rm(self, key): # delete entry
+    del self.db[key] 
 
